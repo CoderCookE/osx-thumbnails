1a2cb365bf21e42f13419b9dd1a8096fc4d962c0
Eric Cook <ericcook@Home-MacBook-Pro.local>
2018-06-22 09:31:22 -0400

    Move thumbnail creation into thumbnail struct

--- main.go
+++ main.go
@@ -4,34 +4,17 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
-	"image"
-	"image/png"
 	"io/ioutil"
 	"log"
 	"os"
 )
 
-type Thumbnail struct {
-	width               int
-	height              int
-	bitspercomponent    int
-	bitsperpixel        int
-	bytesperrow         int
-	bitmapdata_location int
-	bitmapdata_length   int
-}
-
 const TMPLOCATION = "./tmp/dat1"
 
 func main() {
 	println("starting")
-	//echo $TMPDIR
 	input := fmt.Sprintf("%s../C/com.apple.QuickLook.thumbnailcache", os.Getenv("TMPDIR"))
-	println(input)
-
 	dbLocation := fmt.Sprintf("%s/index.sqlite", input)
-
-	println(dbLocation)
 
 	data, err := ioutil.ReadFile(dbLocation)
 	if err != nil {
@@ -64,19 +47,8 @@ func main() {
 	var bitspercomponent int
 	var bitsperpixel int
 	var bytesperrow int
-	var bitmapdata_location int
-	var bitmapdata_length int
-
-	thumbnails := make([]*Thumbnail, 0)
-	for rows.Next() {
-		err = rows.Scan(&width, &height, &bitspercomponent, &bitsperpixel, &bytesperrow, &bitmapdata_location, &bitmapdata_length)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		thumbnail := &Thumbnail{width, height, bitspercomponent, bitsperpixel, bytesperrow, bitmapdata_location, bitmapdata_length}
-		thumbnails = append(thumbnails, thumbnail)
-	}
+	var bitmapdata_location int64
+	var bitmapdata_length int64
 
 	dataFile := fmt.Sprintf("%s/thumbnails.data", input)
 	f, err := os.Open(dataFile)
@@ -85,22 +57,29 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for i, thumb := range thumbnails {
-		dataLocation := int64(thumb.bitmapdata_location)
-		dataLength := int64(thumb.bitmapdata_length)
-
-		buf := make([]byte, dataLength)
-		_, err = f.ReadAt(buf, dataLocation)
+	thumbnails := make([]*Thumbnail, 0)
+	for rows.Next() {
+		err = rows.Scan(&width, &height, &bitspercomponent, &bitsperpixel, &bytesperrow, &bitmapdata_location, &bitmapdata_length)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		img := &image.RGBA{Pix: buf, Stride: thumb.bytesperrow, Rect: image.Rect(0, 0, thumb.width, thumb.height)}
+		thumbnail := &Thumbnail{width, height, bitspercomponent, bitsperpixel, bytesperrow, bitmapdata_location, bitmapdata_length, nil}
+		thumbnail.FetchData(f)
+		thumbnails = append(thumbnails, thumbnail)
+	}
 
-		filename := fmt.Sprintf("./output/out-%d.png", i)
-		out, _ := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0600)
-		defer out.Close()
+	total := len(thumbnails)
+	var successCount int
+	for i, thumb := range thumbnails {
 
-		png.Encode(out, img)
+		if thumb.data != nil {
+			filename := fmt.Sprintf("./output/out-%d.png", i)
+			thumb.CreateImage(filename)
+			successCount += 1
+			log.Printf("Successfully created %d of %d thumbnails", successCount, total)
+		} else {
+			log.Print("Image creation failed, miss data")
+		}
 	}
 }
---
+++ thumbnail.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"image"
+	"image/png"
+	"log"
+	"os"
+)
+
+type Thumbnail struct {
+	width               int
+	height              int
+	bitspercomponent    int
+	bitsperpixel        int
+	bytesperrow         int
+	bitmapdata_location int64
+	bitmapdata_length   int64
+	data                []byte
+}
+
+func (thumb *Thumbnail) FetchData(f *os.File) {
+	buf := make([]byte, thumb.bitmapdata_length)
+	_, err := f.ReadAt(buf, thumb.bitmapdata_location)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	thumb.data = buf
+}
+
+func (thumb *Thumbnail) CreateImage(output string) {
+	img := &image.RGBA{Pix: thumb.data, Stride: thumb.bytesperrow, Rect: image.Rect(0, 0, thumb.width, thumb.height)}
+
+	out, _ := os.OpenFile(output, os.O_WRONLY|os.O_CREATE, 0600)
+	defer out.Close()
+
+	png.Encode(out, img)
+}
